969f68365ad61fdcc790008feb946f3bcf378c5f
astrlux <37908451+astrlux@users.noreply.github.com>
2022-04-01 18:48:25 -0700

    [housekeeping] remove shipwright_build.lua

--- shipwright_build.lua
+++
@@ -1,8 +0,0 @@
-local colorscheme = require './lua/lush_theme/karonda'
-local lushwright = require 'shipwright.transform.lush'
-
-run(colorscheme, lushwright.to_vimscript,
-
-    {append, {'set background=dark', 'let g:colors_name="karonda"'}},
-
-    {overwrite, 'colors/karonda.vim'})
